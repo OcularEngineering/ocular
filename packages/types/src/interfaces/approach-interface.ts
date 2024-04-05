2681a879ea4d0e643deee234b43552198c3b26e4
Louis Murerwa <louismurerwa97@gmail.com>
2024-04-05 12:25:50 -0700

    Add Search Approach

--- packages/types/src/interfaces/approach-interface.ts
+++ packages/types/src/interfaces/approach-interface.ts
@@ -1,16 +1,12 @@
-import { IndexableDocChunk, Message, SearchResult } from '../common';
+import { IndexableDocChunk, Message, SearchResult, SearchResultChunk } from '../common';
 import { SearchContext } from "../common";
 
 export enum ApproachDefinitions {
   ASK_RETRIEVE_READ="ask-retrieve-read",
   CHAT_RETRIEVE_READ="chat-retrieve-read",
 }
-export interface IChatApproach {
-  identifier: ApproachDefinitions;
-  run(indexName: string, messages: Message[], context?: SearchContext): Promise<SearchResult>;
-}
-
-export interface IAskApproach {
+export interface IApproach {
   identifier: ApproachDefinitions;
   run(indexName: string, query: string, context?: SearchContext): Promise<SearchResult>;
+  runWithStreaming(query: string, context?: SearchContext): AsyncGenerator<SearchResultChunk, void>;
 }
