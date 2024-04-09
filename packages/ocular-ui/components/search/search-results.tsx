b3d0167ffc1cdd91c3199fbcba02469b74662737
Michael Moyo <michaelmushabatimoyo@gmail.com>
2024-04-09 02:02:32 -0700

    Smooth transition to sources

--- packages/ocular-ui/components/search/search-results.tsx
+++ packages/ocular-ui/components/search/search-results.tsx
@@ -8,11 +8,17 @@ import {
 import PaginationButtons from "./pagination-buttons";
 import AppFilterOptions from "./app-filter-options";
 import ReactMarkdown from 'react-markdown';
+import { Separator } from '@/components/ui/separator';
+import {
+  ChevronDownIcon,
+} from "@heroicons/react/outline";
 
 // AI Results Component
 export const AIResults = ({ content, search_results }) => {
+  const [showResults, setShowResults] = useState(false);
+
   return (
-    <div className='flex flex-col rounded-xl px-6 py-4 max-w-4xl'>
+    <div className='flex flex-col rounded-xl px-6 py-4 max-w-4xl space-y-3'>
       <div className='flex flex-row'>
         <div className="flex items-center space-x-2">
           <Image src="/Ocular-logo-light.svg" alt="Ocular Copilot" className="size-[50px]" width={50} height={10} /> 
@@ -20,24 +26,33 @@ export const AIResults = ({ content, search_results }) => {
         </div>
       </div>
       <ReactMarkdown className="font-regular text-md space-y-4">{content}</ReactMarkdown>
-      <div className="flex flex-row space-x-5 mt-5 overflow-auto scrollbar-hide">
-        {search_results.slice(0, 10).map((result: any, index: any) => (
-          <div
-            className="bg-gray-100 flex flex-row rounded-2xl p-4 text-xs sm:text-base w-[200px]  flex-none"
-            key={index}
-          >
-            <div className='space-y-1 overflow-hidden'>
-              <a href={result.location} target="_blank" rel="noopener noreferrer" className='flex flex-row space-x-0'>
-                <Image src={result && result.source === 'pagerduty' ? '/PagerDuty.png' : result && result.source ? `/${result.source}.svg` : '/default.png'} alt={result.title} className="mr-4 size-[20px]" width={10} height={10} />
-                <h3 className="text-sm mb-2 truncate font-semibold text-blue-800 group-hover:underline dark:text-blue-400">
-                  {result.source.charAt(0).toUpperCase() + result.source.slice(1)}
-                </h3>
-              </a>
-              <p className="font-regular line-clamp-1 text-sm" dangerouslySetInnerHTML={{ __html: result.content }}></p>
-            </div>
+      <div className='mt-2 mb-3'>
+        <button onClick={() => setShowResults(!showResults)}>
+          <div className='flex flex-row bg-blue-100/50 p-2 px-3 rounded-2xl text-sm font-semibold items-center gap-2'>
+            {search_results.length} sources
+          <ChevronDownIcon className={`h-4 ${showResults ? 'rotate-180' : ''}`} />
           </div>
-        ))}
+        </button>
+        <div className={`flex flex-row space-x-5 mt-5 overflow-auto scrollbar-hide transition-all duration-300 ${showResults ? 'opacity-100 max-h-[1000px]' : 'opacity-0 max-h-0'}`}>
+          {search_results.slice(0, 13).map((result: any, index: any) => (
+            <div
+              className="bg-blue-100/50 flex flex-row rounded-2xl p-4 text-xs sm:text-base w-[200px]  flex-none"
+              key={index}
+            >
+              <div className='space-y-1 overflow-hidden'>
+                <a href={result.location} target="_blank" rel="noopener noreferrer" className='flex flex-row space-x-0'>
+                  <Image src={result && result.source === 'pagerduty' ? '/PagerDuty.png' : result && result.source ? `/${result.source}.svg` : '/default.png'} alt={result.title} className="mr-4 size-[20px]" width={10} height={10} />
+                  <h3 className="text-sm mb-2 truncate font-semibold text-blue-800 group-hover:underline dark:text-blue-400">
+                    {result.source.charAt(0).toUpperCase() + result.source.slice(1)}
+                  </h3>
+                </a>
+                <p className="font-regular line-clamp-1 text-sm" dangerouslySetInnerHTML={{ __html: result.content }}></p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <Separator />
     </div>
   );
 };
@@ -63,7 +78,9 @@ const Results = ({ results }) => (
               </a>
               <p className="font-regular line-clamp-3 text-sm text-gray-500">
                 {
-                  new Date(result.updated_at).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' })
+                  !isNaN(new Date(result.updated_at).getTime()) ?
+                  new Date(result.updated_at).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' }) 
+                  : null
                 }
               </p>
               <p className="font-regular line-clamp-3 text-sm" dangerouslySetInnerHTML={{ __html: result.content }}></p>
@@ -118,8 +135,8 @@ export default function SearchResults({ search_results, ai_content  }) {
     <div className="font-open-sans dark:bg-primary-dark mx-auto flex min-h-screen w-full flex-col gap-10 px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52  dark:text-white" style={{background: 'linear-gradient(to bottom, rgba(0, 0, 255, 0.015) 1%, transparent)'}} >
       <AIResults content={ai_content} search_results={search_results} />
       <div className='flex flex-row'>
-      <Results results={search_results} />
-      <ResultsFilter results={search_results} />
+        <Results results={search_results} />
+        <ResultsFilter results={search_results} />
       </div>
     </div>
   );
