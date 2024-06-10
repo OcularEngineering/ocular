a8e9d5b3418e262ed337df400812b02e135ffa2b
Michael Moyo <michaelmushabatimoyo@gmail.com>
2024-06-10 18:39:49 -0400

    Adding details to dialog

--- packages/ocular-ui/components/files/files-dialog.tsx
+++ packages/ocular-ui/components/files/files-dialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from 'next/router';
 import { buttonVariants, Button } from "@/components/ui/button";
 import {
@@ -22,72 +21,77 @@ import {
 import {
   File
 } from "lucide-react"
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
+
+import SectionContainer from "@/components/section-container"
+import { UploadedFilesCard } from "@/components/files/uploaded-files-card"
+import { useUploadFile } from "@/lib/hooks/files/use-upload-file"
+import { FileUploader } from "@/components/files/file-uploader"
 
 export default function FilesDialog({ link }) {
   const router = useRouter();
   const isActive = (href) => router.pathname === href;
 
+  const { uploadFiles, progresses, uploadedFiles, isUploading } = useUploadFile(
+    "fileUploader",
+    { defaultUploadedFiles: [] }
+  )
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Tooltip delayDuration={0}>
-          <div className="flex items-center space-x-3">
-            <TooltipTrigger asChild>
-              <div
-                className={`${
-                  buttonVariants({ variant: link.variant, size: "icon" })
-                } h-9 w-9 ${
-                  isActive(link.link) ? 'bg-accent rounded-md' : 'bg-transparent'
-                } ${
-                  link.variant === "default" && "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-                }`}
-              >
-                <File style={{ height: '19px', width: '19px' }} /> 
-                <span className="sr-only">{link.title}</span>
+      <Tooltip>
+        <div className="flex items-center space-x-3">
+          <TooltipTrigger asChild>
+            <DialogTrigger asChild>
+              <div className="flex items-center space-x-3 cursor-pointer">
+                <div
+                  className={`${
+                    buttonVariants({ variant: link.variant, size: "icon" })
+                  } h-9 w-9 ${
+                    isActive(link.link) ? 'bg-accent rounded-md' : 'bg-transparent'
+                  } ${
+                    link.variant === "default" && "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                  }`}
+                >
+                  <File style={{ height: '19px', width: '19px' }} />
+                  <span className="sr-only">{link.title}</span>
+                </div>
               </div>
-            </TooltipTrigger>
-            <TooltipContent side="right" className="flex items-center gap-4">
-              {link.title}
-              {link.label && (
-                <span className="text-muted-foreground ml-auto">
-                  {link.label}
-                </span>
-              )}
-            </TooltipContent>
-          </div>
-        </Tooltip>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Share link</DialogTitle>
-          <DialogDescription>
-            Anyone who has this link will be able to view this.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex items-center space-x-2">
-          <div className="grid flex-1 gap-2">
-            <Label htmlFor="link" className="sr-only">
-              Link
-            </Label>
-            <Input
-              id="link"
-              defaultValue="https://ui.shadcn.com/docs/installation"
-              readOnly
-            />
-          </div>
-          <Button type="submit" size="sm" className="px-3">
-            <span className="sr-only">Copy</span>
-          </Button>
+            </DialogTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="right" className="flex items-center gap-4">
+            {link.title}
+            {link.label && (
+              <span className="text-muted-foreground ml-auto">
+                {link.label}
+              </span>
+            )}
+          </TooltipContent>
         </div>
-        <DialogFooter className="sm:justify-start">
+      </Tooltip>
+
+      <DialogContent className="w-[60vw] h-[90vh] ">
+        <DialogHeader>
+        </DialogHeader>
+        
+        <div>
+          <FileUploader
+            maxFiles={Infinity}
+            maxSize={Infinity}
+            progresses={progresses}
+            onUpload={uploadFiles}
+            disabled={isUploading}
+          />
+          <UploadedFilesCard uploadedFiles={uploadedFiles} />
+        </div>
+
+        {/* <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
               Close
             </Button>
           </DialogClose>
-        </DialogFooter>
+        </DialogFooter> */}
       </DialogContent>
     </Dialog>
   );
