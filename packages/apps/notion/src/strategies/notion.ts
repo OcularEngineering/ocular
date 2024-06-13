f7bf309ad4e4b1b6104771061a2f7f70113bf428
Vivek Lahole <viveklahole2020@gmail.com>
2024-06-14 03:42:48 +0530

    Notion : Pages Indexing , Cleanup , Recursive ppage crawling

--- packages/apps/notion/src/strategies/notion.ts
+++ packages/apps/notion/src/strategies/notion.ts
@@ -1,7 +1,11 @@
 import { BatchJobService, Organisation, QueueService } from "@ocular/ocular";
 import NotionService from "../services/notion";
-import { INDEX_DOCUMENT_EVENT, APPS_INDEXING_TOPIC } from "@ocular/types";
-import { AbstractBatchJobStrategy } from "@ocular/types";
+import {
+  INDEX_DOCUMENT_EVENT,
+  APPS_INDEXING_TOPIC,
+  Logger,
+  AbstractBatchJobStrategy,
+} from "@ocular/types";
 
 export default class NotionStrategy extends AbstractBatchJobStrategy {
   static identifier = "notion-indexing-strategy";
@@ -9,24 +13,43 @@ export default class NotionStrategy extends AbstractBatchJobStrategy {
   protected batchJobService_: BatchJobService;
   protected notionService_: NotionService;
   protected queueService_: QueueService;
+  protected logger_: Logger;
 
   constructor(container) {
     super(arguments[0]);
     this.notionService_ = container.notionService;
     this.batchJobService_ = container.batchJobService;
     this.queueService_ = container.queueService;
+    this.logger_ = container.logger;
   }
 
   async processJob(batchJobId: string): Promise<void> {
     const batchJob = await this.batchJobService_.retrieve(batchJobId);
-    const stream = await this.notionService_.getNotionPagesData(
-      batchJob.context?.org as Organisation
+    const org = batchJob.context?.org as Organisation;
+    // Start Tracking The Activity of The Indexing Process
+    const oculationNotionPagesActivity = this.logger_.activity(
+      `processJob: Oculating Notion Data for organisation: ${org.id} name: ${org.name}`
     );
+
+    const stream = await this.notionService_.notionIndexableDocuments(org);
     stream.on("data", (documents) => {
       this.queueService_.sendBatch(APPS_INDEXING_TOPIC, documents);
     });
+
+    // Log The End of The Indexing Process
     stream.on("end", () => {
-      console.log("No more data");
+      this.logger_.success(
+        oculationNotionPagesActivity,
+        `processJob:Starting oculation of Notion Pages for ${org.id} organisation`
+      );
+    });
+
+    // Log The Error of The Indexing Process
+    stream.on("end", () => {
+      this.logger_.error(
+        oculationNotionPagesActivity,
+        `processJob:Starting oculation of Notion Pages for ${org.id} organisation`
+      );
     });
   }
 
