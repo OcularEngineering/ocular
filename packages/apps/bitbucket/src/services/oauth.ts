741f90fc195764a146bdbd031fbe31a34500b9d0
Hitesh Wadhwani <hiteshwadhwani1403@gmail.com>
2024-07-09 23:51:08 +0530

    removed auth folder and created api-token-service in utils folder;

--- packages/apps/bitbucket/src/services/oauth.ts
+++ packages/apps/bitbucket/src/services/oauth.ts
@@ -1,1 +1,132 @@
-export {default} from "../auth/oauth"
+import axios from "axios";
+import {
+  AppauthorizationService,
+  AppNameDefinitions,
+  AppCategoryDefinitions,
+  AuthToken,
+  AuthStrategy,
+  TokenTypes,
+} from "@ocular/types";
+import { ConfigModule } from "@ocular/ocular/src/types";
+
+class BitBucketOauth extends AppauthorizationService {
+  protected client_id_: string;
+  protected client_secret_: string;
+  protected configModule_: ConfigModule;
+  protected redirect_uri_: string;
+  protected auth_strategy_: AuthStrategy
+
+  constructor(container, options) {
+    super(arguments[0]);
+    this.client_id_ = options.client_id;
+    this.client_secret_ = options.client_secret;
+    this.redirect_uri_ = options.redirect_uri;
+    this.configModule_ = container.configModule;
+    this.auth_strategy_ = options.auth_strategy;
+  }
+
+  static getAppDetails(projectConfig, options) {
+    const client_id = options.client_id;
+    const client_secret = options.client_secret;
+    const redirect = options.redirect_uri;
+    const auth_strategy = options.auth_strategy;
+    return {
+      name: AppNameDefinitions.BITBUCKET,
+      logo: "/bitbucket.svg",
+      description:
+        "Bitbucket is a Git-based source code repository hosting service owned by Atlassian",
+      oauth_url: `https://bitbucket.org/site/oauth2/authorize?client_id=${client_id}&response_type=code.`,
+      slug: AppNameDefinitions.BITBUCKET,
+      category: AppCategoryDefinitions.FILE_STORAGE,
+      developer: "Ocular AI",
+      images: ["/bitbucket.svg"],
+      auth_strategy: auth_strategy,
+      overview:
+        "Bitbucket is a Git-based source code repository hosting service owned by Atlassian",
+      docs: "https://developer.atlassian.com/cloud/bitbucket/rest",
+      website: "https://bitbucket.org/product/",
+    };
+  }
+
+  async refreshToken(refresh_token: string): Promise<AuthToken> {
+    const body = {
+      grant_type: "refresh_token",
+      client_id: this.client_id_,
+      client_secret: this.client_secret_,
+      refresh_token: refresh_token,
+    };
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+
+    return axios
+      .post(
+        `https://bitbucket.org/site/oauth2/access_token?grant_type=refresh_token&refresh_token=${refresh_token}`,
+        body,
+        config
+      )
+      .then((res) => {
+        return {
+          token: res.data.access_token,
+          token_expires_at: new Date(Date.now() + res.data.expires_in * 1000),
+          refresh_token: res.data.refresh_token,
+        } as AuthToken;
+      })
+      .catch((err) => {
+        console.error(err);
+        throw err;
+      });
+  }
+
+  async generateToken(code: string): Promise<AuthToken> {
+    console.log("***** Generating token from the code:\n");
+
+    if(this.auth_strategy_ === AuthStrategy.API_TOKEN_STRATEGY){
+      // BitBucket access token does not have any expiry
+      return {
+        type: TokenTypes.BEARER,
+        auth_strategy: AuthStrategy.API_TOKEN_STRATEGY,
+        token: code,
+        refresh_token: "NO_REFRESH_TOKEN",
+      } as AuthToken
+    }
+
+    const body = {
+      grant_type: "authorization_code",
+      client_id: `${this.client_id_}`,
+      client_secret: `${this.client_secret_}`,
+      code: code,
+      redirect_uri: `${this.redirect_uri_}`,
+    };
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    return axios
+      .post(
+        `https://bitbucket.org/site/oauth2/access_token?grant_type=authorization_code&code=${code}`,
+        body,
+        config
+      )
+      .then((res) => {
+        return {
+          type: res.data.token_type,
+          token: res.data.access_token,
+          token_expires_at: new Date(Date.now() + res.data.expires_in * 1000),
+          refresh_token: res.data.refresh_token,
+        } as AuthToken;
+      })
+      .catch((err) => {
+        console.error(err);
+        throw err;
+      });
+  }
+}
+
+export default BitBucketOauth;
---
+++ packages/apps/bitbucket/src/utils/api-token-service.ts
@@ -0,0 +1,296 @@
+import axios from "axios";
+import { Readable } from "stream";
+import {
+  AppAuthorizationService,
+  Organisation,
+  RateLimiterService,
+} from "@ocular/ocular";
+import {
+  AppNameDefinitions,
+  DocType,
+  IndexableDocument,
+  Logger,
+  Section,
+  TransactionBaseService,
+  ApiConfig,
+  AuthStrategy,
+} from "@ocular/types";
+import { ConfigModule } from "@ocular/ocular/src/types";
+import { RateLimiterQueue } from "rate-limiter-flexible";
+
+export default class ApiTokenService extends TransactionBaseService {
+  protected appAuthorizationService_: AppAuthorizationService;
+  protected logger_: Logger;
+  protected container_: ConfigModule;
+  protected rateLimiterService_: RateLimiterService;
+  protected requestQueue_: RateLimiterQueue;
+
+  constructor(container) {
+    super(arguments[0]);
+    this.appAuthorizationService_ = container.appAuthorizationService;
+    this.logger_ = container.logger;
+    this.container_ = container;
+    this.rateLimiterService_ = container.rateLimiterService;
+    this.requestQueue_ = this.rateLimiterService_.getRequestQueue(
+      AppNameDefinitions.BITBUCKET
+    );
+  }
+
+  async getBitBucketData(org: Organisation) {
+    return Readable.from(this.getBitBucketInformation(org));
+  }
+
+  async *getBitBucketInformation(
+    org: Organisation
+  ): AsyncGenerator<IndexableDocument[]> {
+    this.logger_.info(
+      `Starting oculation of BitBucket for ${org.id} organisation`
+    );
+
+    // Get BitBucket AuthToken for the organisation
+    const auth = await this.appAuthorizationService_.retrieve({
+      id: org.id,
+      app_name: AppNameDefinitions.BITBUCKET,
+    });
+
+    if (!auth) {
+      this.logger_.error(`No Bitbucket Auth found for ${org.id} organisation`);
+      return;
+    }
+
+    const config: ApiConfig = {
+      headers: {
+        Authorization: `Bearer ${auth.token}`,
+        Accept: "application/json",
+      },
+    };
+
+    let documents: IndexableDocument[] = [];
+
+    try {
+    if(!auth?.metadata?.workspace){
+        throw new Error("No workspace found in metadata")
+    }
+    const workspace = await this.fetchWorkspace(auth?.metadata?.workspace as string, config)
+    const workspaces = [workspace]
+      for (const workspace of workspaces) {
+        const repositories = await this.fetchRepositoriesForWorkspace(
+          workspace.slug,
+          config
+        );
+        for (const repository of repositories) {
+          const prs = await this.fetchPRForRepositories(
+            workspace.slug,
+            repository.uuid,
+            config
+          );
+          for (const pr of prs) {
+            // add comment section reference in section tag
+            const comments = await this.fetchCommentsForPR(
+              workspace.slug,
+              repository.uuid,
+              pr.id,
+              config
+            );
+            const sections: Section[] = comments.map((comment, index) => ({
+              offset: index,
+              content: comment.type,
+              link: `https://api.bitbucket.org/2.0/repositories/${workspace.slug}/${repository.uuid}/pullrequests/${pr.id}/comments`,
+            }));
+            const prDoc: IndexableDocument = {
+              id: pr.id,
+              organisationId: org.id,
+              source: AppNameDefinitions.BITBUCKET,
+              title: pr.title,
+              metadata: {
+                repository_id: repository.uuid,
+                workspace_id: workspace.uuid,
+              },
+              sections: sections,
+              type: DocType.TXT,
+              updatedAt: new Date(Date.now()),
+            };
+            documents.push(prDoc);
+            if (documents.length >= 100) {
+              yield documents;
+              documents = [];
+            }
+          }
+
+          const issues = await this.fetchIssueForRepositories(
+            workspace.slug,
+            repository.uuid,
+            config
+          );
+          for (const issue of issues) {
+            // add comment section reference in section tag
+            const comments = await this.fetchCommentsForIssue(
+              workspace.slug,
+              repository.uuid,
+              issue.id,
+              config
+            );
+            const sections: Section[] = comments.map((comment, index) => ({
+              offset: index,
+              content: comment.type,
+              link: `https://api.bitbucket.org/2.0/repositories/${workspace.slug}/${repository.uuid}/issues/${issue.id}/comments`,
+            }));
+            const issueDoc: IndexableDocument = {
+              id: issue.id,
+              organisationId: org.id,
+              source: AppNameDefinitions.BITBUCKET,
+              title: issue.title,
+              metadata: {
+                repository_id: repository.uuid,
+                workspace_id: workspace.uuid,
+              },
+              sections: sections,
+              type: DocType.TXT,
+              updatedAt: new Date(Date.now()),
+            };
+            documents.push(issueDoc);
+            if (documents.length >= 100) {
+              yield documents;
+              documents = [];
+            }
+          }
+        }
+      }
+      yield documents;
+      await this.appAuthorizationService_.update(auth.id, {
+        last_sync: new Date(),
+      });
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        // Check if it's an unauthorized error
+        this.logger_.info(
+          `Refreshing bitbucket token for ${org.id} organisation`
+        );
+
+        // Refresh the token
+        const authToken = await this.container_["bitbucketOauth"].refreshToken(
+          auth.refresh_token
+        );
+
+        // Update the Auth record with the new token
+        await this.appAuthorizationService_.update(auth.id, authToken);
+
+        // Retry the request
+        return this.getBitBucketInformation(org);
+      } else {
+        throw new Error(error);
+      }
+    }
+
+    this.logger_.info(
+      `Finished oculation of BitBucket for ${org.id} organisation`
+    );
+  }
+
+  async fetchWorkspace(workspaceName: string, config: ApiConfig){
+    try{
+      await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+      const url = `https://api.bitbucket.org/2.0/workspaces/${workspaceName}`
+      const workspace = await axios.get(url, config)
+      if(workspace.status !== 200) throw new Error(`Failed to fetch workspace ${workspaceName}`);
+      return workspace.data
+    }
+    catch(error){
+      throw new Error(`Failed to fetch workspace ${workspaceName}`);
+    }
+  }
+
+  async fetchRepositoriesForWorkspace(
+    workspace_slug: string,
+    config: ApiConfig
+  ) {
+    try {
+      await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+      const repoEndpoint = await axios.get(
+        `https://api.bitbucket.org/2.0/repositories/${workspace_slug}`,
+        config
+      );
+      const repoArray = repoEndpoint.data.values || [];
+      return repoArray;
+    } catch (err) {
+      throw new Error("Repositoes not able to fetch");
+    }
+  }
+
+  async fetchPRForRepositories(
+    workspace_slug: string,
+    repo_slug: string,
+    config: ApiConfig
+  ) {
+    await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+    try {
+      const prEndpoint = await axios.get(
+        `https://api.bitbucket.org/2.0/repositories/${workspace_slug}/${repo_slug}/pullrequests`,
+        config
+      );
+      const prArray = prEndpoint.data.values || [];
+      return prArray;
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  }
+  async fetchIssueForRepositories(
+    workspace_slug: string,
+    repo_slug: string,
+    config: ApiConfig
+  ) {
+    await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+    try {
+      const issueEndpoint = await axios.get(
+        `https://api.bitbucket.org/2.0/repositories/${workspace_slug}/${repo_slug}/issues`,
+        config
+      );
+      const issueArray = issueEndpoint.data.values || [];
+      return issueArray;
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  }
+
+  async fetchCommentsForIssue(
+    workspace_slug: string,
+    repo_slug: string,
+    issue_id: string,
+    config: ApiConfig
+  ) {
+    await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+    try {
+      const commentsEndpoint = await axios.get(
+        `https://api.bitbucket.org/2.0/repositories/${workspace_slug}/${repo_slug}/issues/${issue_id}/comments`,
+        config
+      );
+      const commentsArray = commentsEndpoint.data.values || [];
+      return commentsArray;
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  }
+
+  async fetchCommentsForPR(
+    workspace_slug: string,
+    repo_slug: string,
+    pr_id: string,
+    config: ApiConfig
+  ) {
+    await this.requestQueue_.removeTokens(1, AppNameDefinitions.BITBUCKET);
+    try {
+      const commentsEndpoint = await axios.get(
+        `https://api.bitbucket.org/2.0/repositories/${workspace_slug}/${repo_slug}/pullrequests/${pr_id}/comments`,
+        config
+      );
+      const commentsArray = commentsEndpoint.data.values || [];
+      return commentsArray;
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  }
+}
